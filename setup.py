609fcad37d8ad75e1a3bb4dbb5986db5e059d73a
Sam Gordon <sam.gordon@csiro.au>
2023-09-11 16:25:32 +1000

    added dummy requirements file

---
+++ setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+"""
+Set up for mymodule
+"""
+from setuptools import setup
+
+import os
+
+def get_requirements():
+    """
+    Read the requirements from a file
+    """
+    requirements = []
+    if os.path.exists('requirements.txt'):
+        with open('requirements.txt') as req:
+            for line in req:
+                # skip commented lines
+                if not line.startswith('#'):
+                    requirements.append(line.strip())
+    return requirements
+
+setup(
+    name='constellation_identifier', # the name of the module
+    packages=['constellation_identifier'], # the location of the module
+    version="0.1",
+    install_requires=get_requirements(),
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts':[
+            'hello_world=mymodule.submod1:hello_world'
+            ]}
+)
